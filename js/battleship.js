d368f053313f6cd09f473edaca8e83563ded8e68
Daniel Mai <danielmai1728@gmail.com>
2018-02-14 19:53:45 -0800

    fix check valid set up

--- js/battleship.js
+++ js/battleship.js
@@ -53,40 +53,45 @@ function setUp(input, playerName) {
   let isValidSetUp = true
 
   //Check if input is valid before proceeding
-  checkValidSetUp()
-
-  if(isValidSetUp){
-    if(player1.name === playerName) {
-      player1.addShip(x, y, length, direction)
-      player1.viewBoard()
-    }
-    else {
-      player2.addShip(x, y, length, direction)
-      player2.viewBoard()
-    }
+  if(player1.name === playerName &&  checkValidSetUp(player1)) {
+    player1.addShip(x, y, length, direction)
+    player1.viewBoard()
+  }
+  else if(player2.name === playerName && checkValidSetUp(player2)) {
+    player2.addShip(x, y, length, direction)
+    player2.viewBoard()
   }
 
-  function checkValidSetUp(){
+
+  function checkValidSetUp(player){
+
+    let boardLength = player.getBoard().length
+    let board = player.getBoard()
     //Coordinates start from 0 to gameBoard length - 1
-    if(x < 0 || x >= player1Board.length || y < 0 || y >= player1Board.length) {
+    if(x < 0 || x >= boardLength || y < 0 || y >= boardLength) {
       isValidSetUp = false
       console.log("Coordinates are out of bound.")
+      return false
     }
     //Ship length must be between 2 and 4
     else if (length < MIN_SHIP_LENGTH || length > MAX_SHIP_LENGTH){
       isValidSetUp = false
       console.log('Ship length must be between 2 and 4')
+      return false
     }
     //Direction must be either 0 or 1
     else if(direction !== 0 && direction !== 1){
       isValidSetUp = false
       console.log('Direction must be either 0 or 1')
+      return false
     }
     //The coordinates are already occupied with a ship
-    else if(player1Board[x][y] !== '-' || player2Board[x][y] !== '-'){
+    else if(board[x][y] !== '-'){
       isValidSetUp = false
       console.log('The coordinates selected are occupied.')
+      return false
     }
+    return true
   }
   return isValidSetUp
 }
@@ -105,16 +110,24 @@ function firing(input, playerName){
   if(isValidFiring){
     if(player1.name === playerName) {
       player1.fire(x, y)
-      player1.viewBoard()
+      player2.viewBoard()
     }
     else {
       player2.fire(x, y)
-      player2.viewBoard()
+      player1.viewBoard()
     }
   }
 
   function checkValidFiring(){
-    if (x === NaN || y === NaN) {
+    if (isNaN(x) || isNaN(y)) {
+      isValidFiring = false
+      console.log('Invalid Coordinates')
+    }
+    else if(x < 0 || x > player1Board.length - 1 || y < 0 || y > player1Board.length - 1){
+      isValidFiring = false
+      console.log('Invalid Coordinates')
+    }
+    else if(array.length > 2){
       isValidFiring = false
       console.log('Invalid Coordinates')
     }
@@ -124,11 +137,39 @@ function firing(input, playerName){
 Player.prototype.fire = function(x, y){
   let coord = `${x},${y}`
   let result = 'MISS!'
-  let allShips = this.getAllShips
-  for(let ship in this.getAllShips){
-    let shipCoords = this.getAllShips[ship]
-    if(shipCoords.hasOwnProperty(coord)){
-      result = 'HIT!'
+  if(this.name === PLAYER_1){
+    console.log('Player 1 Attack!')
+    let allShips = player2.getAllShips
+    console.log('player 2 ships')
+    console.log(allShips)
+    for(let ship in allShips){
+      let shipCoords = allShips[ship]
+      if(shipCoords.hasOwnProperty(coord)){
+        player2Board[x][y] = 'X'
+        result = 'HIT!'
+      }
+
+      else {
+        player2Board[x][y] = 'M'
+      }
+    }
+  }
+
+  if(this.name === PLAYER_2){
+    console.log('Player 2 Attack!')
+    let allShips = player1.getAllShips
+    console.log('player 1 ships')
+    console.log(allShips)
+    for(let ship in allShips){
+      let shipCoords = allShips[ship]
+
+      if(shipCoords.hasOwnProperty(coord)){
+        player1Board[x][y] = 'X'
+        result = 'HIT!'
+      }
+      else {
+        player1Board[x][y] = 'M'
+      }
     }
   }
   console.log(result)
@@ -146,39 +187,45 @@ function Player(name){
 Player.prototype.addShip = function(x, y, length, direction) {
   if(this.name === PLAYER_1){
     this.getAllShips['ship' + this.shipCount] = {}
-    for(let i = 0; i < length; i++){
-      let ar = [x, y + i]
-      this.getAllShips['ship' + this.shipCount][ar.toString()] = 'X'
-    }
+
     //Place ship on the board
+
+    //Horizontal ship
     if (direction === 0) {
       for(let i = 0; i < length; i++){
         player1Board[x][y + i] = 'O'
+        let ar = [x, y + i]
+        this.getAllShips['ship' + this.shipCount][ar.toString()] = 'O'
       }
     }
-
+    //Vertical ship
     if(direction === 1){
       for(let i = 0; i < length; i++){
         player1Board[x + i][y] = 'O'
+        let ar = [x + i, y]
+        this.getAllShips['ship' + this.shipCount][ar.toString()] = 'O'
       }
     }
   }
   if(this.name === PLAYER_2){
     this.getAllShips['ship' + this.shipCount] = {}
-    for(let i = 0; i < length; i++){
-      let ar = [x, y + i]
-      this.getAllShips['ship' + this.shipCount][ar.toString()] = 'O'
-    }
+
     //Place ship on the board
+
+    //Horizontal ship
     if (direction === 0) {
       for(let i = 0; i < length; i++){
         player2Board[x][y + i] = 'O'
+        let ar = [x, y + i]
+        this.getAllShips['ship' + this.shipCount][ar.toString()] = 'O'
       }
     }
-
+    //Vertical ship
     if(direction === 1){
       for(let i = 0; i < length; i++){
         player2Board[x + i][y] = 'O'
+        let ar = [x + i, y]
+        this.getAllShips['ship' + this.shipCount][ar.toString()] = 'O'
       }
     }
   }
@@ -187,6 +234,16 @@ Player.prototype.addShip = function(x, y, length, direction) {
 //Get a single ship information
 Player.prototype.getShip = function(id) {
   return this.getAllShips['ship' + id]
+}
+
+//Get player board
+Player.prototype.getBoard = function(){
+  if(this.name === PLAYER_1){
+    return player1Board
+  }
+  else if(this.name === PLAYER_2){
+    return player2Board
+  }
 }
 //View player BOARD
 Player.prototype.viewBoard = function(){
@@ -211,7 +268,7 @@ Player.prototype.viewBoard = function(){
 // ----------------------------------------------
 
 
-let totalShips = 0;
+let totalShips = 0, attackTurn = 0
 
 
 function promptInput(prompt, handler) {
@@ -248,28 +305,32 @@ function promptInput(prompt, handler) {
   //FIRING PHASE
   else {
     console.log('FIRING PHASE!')
-    // prompt = FIRING_PROMPT
+    if (attackTurn === 0) {
+      prompt = FIRING_PROMPT
+    }
+    else {
+      prompt = prompt.replace('Player 1', 'Player 2')
+    }
     rl.question(prompt, input => {
       if (handler(input) !== false) {
         if (prompt.includes('Player 1')) {
           if(firing(input, PLAYER_1)){
-            console.log('OMG!')
-            promptInput(prompt.replace('Player 1', 'Player 2'), handler)
+            attackTurn = 1;
+            promptInput(prompt, handler)
           }
           else {
-            // console.log(INVALID_PROMPT)
-            console.log('NANANA')
-            promptInput(FIRING_PROMPT, handler)
+            console.log(INVALID_PROMPT)
+            promptInput(prompt, handler)
           }
         }
         else {
-          console.log('Player 2 turn!!!')
           if(firing(input, PLAYER_2)){
-            promptInput(FIRING_PROMPT, handler)
+            attackTurn = 0
+            promptInput(prompt, handler)
           }
           else {
-            // console.log(INVALID_PROMPT)
-            promptInput(FIRING_PROMPT.replace('Player 1', 'Player 2'), handler)
+            console.log(INVALID_PROMPT)
+            promptInput(prompt, handler)
           }
         }
       } else {
