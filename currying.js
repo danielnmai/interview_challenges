cec53a207e61808d01900e18da6267678f58543f
Daniel Mai <danielmai1728@gmail.com>
2018-03-04 19:23:02 -0800

    add currying js prep

---
+++ currying.js
@@ -0,0 +1,22 @@
+const greet = function(greeting){
+  return function(name){
+    console.log(greeting + ', ' + name)
+  }
+}
+
+greet('Hello')('Daniel')
+
+var curryIt = function(uncurried) {
+  var parameters = Array.prototype.slice.call(arguments, 1);
+  return function() {
+    return uncurried.apply(this, parameters.concat(
+      Array.prototype.slice.call(arguments, 0)
+    ));
+  };
+};
+
+var greeter = function(greeting, separator, emphasis, name) {
+  console.log(greeting + separator + name + emphasis);
+};
+var greetHello = curryIt(greeter, "Hello", ", ", ".");
+greetHello("Heidi"); //"Hello, Heidi."
