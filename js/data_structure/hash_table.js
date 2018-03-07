8ffbf2c1ced6e7bd02ea529d6a98412520290d8e
Daniel Mai <danielmai1728@gmail.com>
2018-03-06 21:24:54 -0800

    finish hash table

---
+++ js/data_structure/hash_table.js
@@ -0,0 +1,25 @@
+function HashTable(size) {
+  this.size = size
+  this.values = Array(size)
+}
+
+HashTable.prototype.add = function(key, value) {
+  let hashedKey = this.hash(key)
+  this.values[hashedKey] = value
+}
+HashTable.prototype.hash = function(key) {
+  let total = 0
+  for (let i = 0; i < key.length; i++) {
+    total += key.charCodeAt(i)
+  }
+  let hashedKey = total % 30
+  return hashedKey
+}
+HashTable.prototype.retrieve = function(key) {
+  let hashedKey = this.hash(key)
+  return this.values[hashedKey]
+}
+
+let ht = new HashTable(30)
+ht.add('long', 'haha')
+console.log(ht.retrieve('long'))
