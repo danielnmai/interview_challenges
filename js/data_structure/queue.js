5e7556206fe4a3b7734a56410c103188432485f8
Daniel Mai <danielmai1728@gmail.com>
2018-03-06 22:18:59 -0800

    add queue

---
+++ js/data_structure/queue.js
@@ -0,0 +1,31 @@
+function Queue(){
+  this.values = {}
+  this.firstEnd = 0
+  this.lastEnd = 0
+}
+
+Queue.prototype.enqueue = function(value){
+  //Add to the end of the queue
+  if(value){
+    this.values[this.firstEnd] = value
+    this.firstEnd++
+  }
+}
+Queue.prototype.dequeue = function() {
+  //if firstEnd == lastEnd, queue is empty, return undefined
+  if(this.firstEnd - this.lastEnd === 0){
+    return undefined
+  }
+  //Remove from the beginning of the Queue
+  let removedValue = this.values[this.lastEnd]
+  delete this.values[this.lastEnd]
+  this.lastEnd ++
+  return removedValue
+}
+
+let queue = new Queue()
+
+queue.enqueue('first')
+queue.enqueue('second')
+
+console.log(queue.dequeue())
