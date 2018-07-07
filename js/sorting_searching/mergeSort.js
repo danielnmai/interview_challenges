d8a4e43b2efb9e2627114851e49d76b255cba037
daniel <danielmai1728@gmail.com>
2018-07-06 21:59:25 -0700

    add merge sort

---
+++ js/sorting_searching/mergeSort.js
@@ -0,0 +1,30 @@
+function mergeSort(a) {
+	let length = a.length;
+	if (length == 1) return a;
+
+	let mid = Math.floor(length / 2);
+
+	let left = a.slice(0, mid);
+	let right = a.slice(mid);
+
+	return merge(mergeSort(left), mergeSort(right));
+}
+function merge(left, right) {
+	let result = [],
+		l = 0,
+		r = 0;
+	let llen = left.length,
+		rlen = right.length;
+
+	while (l < llen && r < rlen) {
+		if (left[l] < right[r]) {
+			result.push(left[l++]);
+		} else {
+			result.push(right[r++]);
+		}
+	}
+	return result.concat(left.slice(l).concat(right.slice(r)));
+}
+
+ar = [9, 2, 7, 6, 1, 11, 2, 1, 4, 5, 7, 20, 90, 10, 5];
+console.log(mergeSort(ar));
