6829fc3f97d8140e1360d28dea204791c2c8bed1
Daniel Mai <danielmai1728@gmail.com>
2018-07-30 21:25:10 -0700

    change tree values to String

--- js/data_structure/tree.js
+++ js/data_structure/tree.js
@@ -22,16 +22,16 @@ function traverseDF(node){
     }
 }
 
-let rootNode = new Node(3)
-nodeA = new Node(9)
-nodeB = new Node(20)
-nodeC = new Node(15)
-nodeD = new Node(7)
-// nodeE = new Node('E')
-// nodeF = new Node('F')
-// nodeG = new Node('G')
-// nodeH = new Node('H')
-// nodeI = new Node('I')
+let rootNode = new Node('root')
+nodeA = new Node('A')
+nodeB = new Node('B')
+nodeC = new Node('C')
+nodeD = new Node('D')
+nodeE = new Node('E')
+nodeF = new Node('F')
+nodeG = new Node('G')
+nodeH = new Node('H')
+nodeI = new Node('I')
 
 //rootNode children
 rootNode.add(nodeA)
@@ -39,17 +39,17 @@ rootNode.add(nodeB)
 
 
 //Node A children & grandchildren
-// nodeB.add(nodeC)
-// nodeB.add(nodeD)
-// nodeC.add(nodeE)
-// nodeD.add(nodeF)
+nodeB.add(nodeC)
+nodeB.add(nodeD)
+nodeC.add(nodeE)
+nodeD.add(nodeF)
 
 //Node B children and grandchildren
 nodeB.add(nodeC)
 nodeB.add(nodeD)
-// nodeG.add(nodeI)
+nodeG.add(nodeI)
 
-console.log(rootNode)
+// console.log(rootNode)
 traverseDF(rootNode)
 
 
