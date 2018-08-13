0c092da1b3ac3b433f0a69d7f94acc119121eb20
Daniel Mai <danielmai1728@gmail.com>
2018-08-13 11:49:21 -0700

    fix spacing

--- js/cracking-the-coding-interview/chap1-array-string/ex-17-RotateMatrix.js
+++ js/cracking-the-coding-interview/chap1-array-string/ex-17-RotateMatrix.js
@@ -5,12 +5,13 @@ function rotateMatrix(matrix){
     let n = matrix.length
     for(let layer = 0; layer < n / 2; layer++){
         let first = layer, last = n - 1 - layer
+        
         for(let i = first; i < last; i++){
             let offset = i - first
             //save top
             let top = matrix[first][i]
             //bottom - > left
-            matrix[last-offset][first] = matrix[last][last - offset]
+            matrix[last - offset][first] = matrix[last][last - offset]
 
             //right -> bottom
             matrix[last][last - offset] = matrix[i][last]
@@ -24,5 +25,5 @@ function rotateMatrix(matrix){
     return true
 }
 
-let matrix = [[1, 2], [3, 4]]
+let matrix = [[1, 2, 3], [3, 4, 6], [7, 8, 9]]
 rotateMatrix(matrix)
