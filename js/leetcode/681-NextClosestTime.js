2ede507b7705d6ee0a11ce040ec36f40b20da835
danielnmai <danielmai1728@gmail.com>
2018-09-03 11:41:09 -0700

    add leetcode problem (medium)

---
+++ js/leetcode/681-NextClosestTime.js
@@ -0,0 +1,65 @@
+function nextClosestTime(time) {
+
+    let ar = time.replace(':', '').split('').map(char => parseInt(char))
+    let h1 = ar[0],
+        h2 = ar[1],
+        m1 = ar[2],
+        m2 = ar[3],
+        min = Math.min(...ar)
+
+    //test digit
+    //to make sure we do not compare matched element with itself,
+    //hence the 2nd condition (i != index of the digit we want to compare)
+
+    //Return the next closest time in string time format
+    function getResult(){
+        return h1.toString() + h2.toString() + ":" + m1.toString() + m2.toString()
+    }
+
+    //m2 digit
+    for(let i = 0; i < ar.length; i++){   
+        if (ar[i] > m2 && i != 3){
+            m2 = ar[i]
+            console.log("found it!");
+            return getResult()
+        }
+    }
+    //m1 Digit
+    for(let i = 0; i < ar.length; i++){   
+        if (ar[i] > m1 && ar[i] <= 5 && i != 2){
+            m1 = ar[i]
+            return getResult()
+        }
+    }
+    //h2 Digit
+    for(let i = 0; i < ar.length; i++){   
+        if (ar[i] > m1 && ar[i] <= 5 && i != 2){
+            m1 = ar[i]
+            return getResult()
+        }
+    }
+    //h1 Digit
+    for(let i = 0; i < ar.length; i++){   
+        if (ar[i] > h1 && ar[i] <= 2 && i != 0){
+            console.log('Found it!!!!')
+            h1 = ar[i]
+            h2 = min
+            m1 = min
+            m2 = min
+            return getResult()
+        }
+    }
+    //If all the above is false, we need to go to the next day time
+    h1 = min
+    h2 = min
+    m1 = min
+    m2 = min
+
+    return getResult()
+}
+
+console.log(nextClosestTime('01:32'))
+console.log(nextClosestTime('19:39'))
+console.log(nextClosestTime('19:44'))
+console.log(nextClosestTime('23:59'))
+console.log(nextClosestTime('18:42'))
