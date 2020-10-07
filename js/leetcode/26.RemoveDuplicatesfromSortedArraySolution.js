d9bd7c4d0efefdea2e77298e08d14ecace5c8c16
Daniel Mai <danielmai1728@gmail.com>
2020-10-07 11:37:09 -0700

    complete 26 leetcode

--- js/leetcode/26.RemoveDuplicatesfromSortedArraySolution.js
+++ js/leetcode/26.RemoveDuplicatesfromSortedArraySolution.js
@@ -9,12 +9,10 @@ var removeDuplicates = function(nums) {
       nums[curPointer + 1] = nums[i];
       curPointer++;
     }
-    nums[i] = null;
   }
-  console.log('nums ', nums);
   return curPointer + 1;
 };
 
 const ar = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 const ar1 = [1, 2];
-console.log(removeDuplicates(ar1));
+console.log(removeDuplicates(ar));
